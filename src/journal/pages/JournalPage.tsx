99e7ba12e0070678b3ae50639d919c34bf5def1d
AChapeton <andres.chapeton0206@gmail.com>
2024-02-05 16:40:58 -0600

    Added ui from view when nothing is selected

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import JournalLayout from '../layout/JournalLayout'
-import { Typography } from '@mui/material'
+import { NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Sint proident aliqua amet duis officia sint deserunt amet dolor qui minim cupidatat. Exercitation sint aute commodo commodo sint elit excepteur. Reprehenderit ad eiusmod esse nostrud ullamco ullamco cupidatat do ex aliquip aliquip minim id velit. Consectetur ea velit duis duis sunt id ad ullamco magna incididunt. Exercitation reprehenderit fugiat magna consequat ullamco. Elit id fugiat enim tempor aliquip quis enim velit qui. Tempor quis officia eiusmod consectetur commodo fugiat irure dolore dolor.
-      </Typography>
+      <NothingSelectedView/>
     </JournalLayout>
   )
 }
