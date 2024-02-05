502497e16f81a37e85f20218dda463eed36a52a5
AChapeton <andres.chapeton0206@gmail.com>
2024-02-05 16:27:33 -0600

    Added JournalLayout

---
+++ src/journal/layout/JournalLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Box, Toolbar } from '@mui/material'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+
+const drawerWidth = 240
+
+export default function JournalLayout({children}) {
+  return (
+    <Box sx={{display: 'flex'}}>
+      <Navbar drawerWidth={drawerWidth}/>
+      
+      <Sidebar drawerWidth={drawerWidth}/>
+
+      <Box component='main' sx={{flexGrow: 1, p: 3}}>
+        <Toolbar/>
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import JournalLayout from '../layout/JournalLayout'
+import { Typography } from '@mui/material'
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+      <Typography>
+        Sint proident aliqua amet duis officia sint deserunt amet dolor qui minim cupidatat. Exercitation sint aute commodo commodo sint elit excepteur. Reprehenderit ad eiusmod esse nostrud ullamco ullamco cupidatat do ex aliquip aliquip minim id velit. Consectetur ea velit duis duis sunt id ad ullamco magna incididunt. Exercitation reprehenderit fugiat magna consequat ullamco. Elit id fugiat enim tempor aliquip quis enim velit qui. Tempor quis officia eiusmod consectetur commodo fugiat irure dolore dolor.
+      </Typography>
+    </JournalLayout>
   )
 }
