1acbd59db84746a789c2d32d17542b95a45b289c
AChapeton <andres.chapeton0206@gmail.com>
2024-02-05 15:57:33 -0600

    Added AuthLayout to create Login and Register pages

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import AuthLayout from '../layout/AuthLayout'
+import { Button, Grid, TextField, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+      <form action="">
+          <Grid container>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField label='Name' type='text' placeholder='Name' fullWidth/>
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField label='Email' type='email' placeholder='mail@example.com' fullWidth/>
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField label='Password' type='password' placeholder='Password' fullWidth/>
+            </Grid>
+            <Grid container spacing={2} sx={{mb: 2 , mt: 1}}>
+              <Grid item xs={12}>
+                <Button variant='contained' fullWidth>
+                  Create account
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction='row' justifyContent='end'>
+              <Typography sx={{mr: 1}}>Do you already have an account?</Typography>
+              <Link component={RouterLink} color='inherit' to="/auth/login">
+                Login
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+    </AuthLayout>
   )
 }
