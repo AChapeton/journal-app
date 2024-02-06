ce5cfe974498ce463603c6058e7f26eed96a24e3
AChapeton <andres.chapeton0206@gmail.com>
2024-02-06 08:32:50 -0600

    Added Image Gallery

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import JournalLayout from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
+import { ImageGallery } from '../components'
 
 export const JournalPage = () => {
   return (
