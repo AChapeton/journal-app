db521fc0d3c5f4cc1657728baa6ffd39007d558a
AChapeton <andres.chapeton0206@gmail.com>
2024-02-06 08:38:06 -0600

    Added "add new note" button

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -2,12 +2,27 @@ import React from 'react'
 import JournalLayout from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 import { ImageGallery } from '../components'
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView/> */}
-      <NoteView/>
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+      <IconButton 
+        size='large' 
+        sx={{
+          color: 'white', 
+          backgroundColor: 'error.main', 
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed', 
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
